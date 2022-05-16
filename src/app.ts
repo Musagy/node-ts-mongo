cddf5252d6414485503ae2caa999d8404c128c12
Diego Musagy <diego.1203.dm@gmail.com>
2022-05-16 18:34:50 -0500

    agregando cors al proyecto 👽👽👽

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import tasksRouter from "./routes/videos";
+import cors from 'cors'
 
 const app = express();
 app.use(express.json()); // middleware que transforma el body de la petición en un objeto json
+app.use(cors())
 
 app.use("/videos",tasksRouter);
 
